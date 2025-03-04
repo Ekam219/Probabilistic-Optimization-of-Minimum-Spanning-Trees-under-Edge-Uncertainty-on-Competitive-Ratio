5127e5abc4544af1b5e79d9b01c5414fdb6ffad1
EkamJot Singh <ekamjotjaggi2511@gmail.com>
2025-03-04 19:26:05 +0530

    removed unwanted edges

--- generator.py
+++ generator.py
@@ -92,54 +92,6 @@ class GraphGenerator:
     def edgeCaseA(self, n, m, weight):
         return self.costructGraph(n, m, weight, weight, weight, 0, 1)
 
-    # All the edges are either lo or hi
-    def edgeCaseB(self, n, m, lo, hi):
-        if lo > hi:
-            lo, hi = hi, lo
-        if n > 1000 or m < n-1 or m > n*(n - 1)//2:
-            assert False
-        if lo < 0 or hi < lo:
-            assert False
-     
-        order = [i for i in range(1, n + 1)]
-        order = np.array(order)
-        random.shuffle(order)
-        order = order.tolist()
-        parent = [-1 for i in range(n + 1)]
-        edgeSet = set()
-        for i in range(1, n):
-            parent[order[i]] = order[random.randint(0, i-1)]
-        edgeFrom, edgeTo, edgeLo, edgeHi, edgeActual = [], [], [], [], []
-        for i in range(1, n):
-            edgeFrom.append(order[i])
-            edgeTo.append(parent[order[i]])
-            edgeSet.add((order[i], parent[order[i]]))
-            edgeSet.add((parent[order[i]], order[i]))
-            edgeLo.append(lo)
-            edgeHi.append(hi)
-            weight = lo
-            if random.randint(0, 1):
-                weight = hi
-            edgeActual.append(weight)
-        m -= (n - 1)
-        for i in range(m):
-            u, v = random.randint(1, n), random.randint(1, n)
-            while u == v or (u, v) in edgeSet:
-                v = random.randint(1, n)
-            edgeFrom.append(u)
-            edgeTo.append(v)
-            edgeSet.add((u, v))
-            edgeSet.add((v, u))
-            edgeLo.append(lo)
-            edgeHi.append(hi)
-            weight = lo
-            if random.randint(0, 1):
-                weight = hi
-            edgeActual.append(weight)
-        graph = UncertainGraph()
-        graph.buildFromParameters(n, edgeFrom, edgeTo, edgeLo, edgeHi, edgeActual)
-        return graph
-
     # Complete Graph
     def edgeCaseC(self, n, lo, hi, centre=None, deviation=0.5, trivialProbability=0.5):
         return self.costructGraph(n, n*(n-1)//2, lo, hi, centre, deviation, trivialProbability)
